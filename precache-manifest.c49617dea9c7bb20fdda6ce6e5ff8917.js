54505d1d8bb5e6a697e188f254211a00ad8d0264
aniket <gdg4dev@gmail.com>
2020-09-01 18:41:19 +0530

    Updates

---
+++ precache-manifest.c49617dea9c7bb20fdda6ce6e5ff8917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae521f5efa5ab5d00ad508275c382f0",
+    "url": "/mondex/index.html"
+  },
+  {
+    "revision": "6aa36203fb4e58bed0bb",
+    "url": "/mondex/static/css/main.5d29b0a2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/mondex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mondex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa36203fb4e58bed0bb",
+    "url": "/mondex/static/js/main.d7fae8f7.chunk.js"
+  },
+  {
+    "revision": "154ccad42a02f15c4677",
+    "url": "/mondex/static/js/runtime-main.52bd10d1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mondex/precache-manifest.c49617dea9c7bb20fdda6ce6e5ff8917.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mondex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
